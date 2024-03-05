aef65abde438229f081cbaebf401a6b3bb58fa65
linus9840 <linus9840@gmail.com>
2024-03-05 18:36:22 +0545

    first three hours in

---
+++ lib/components/body.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/models/product.dart';
+import 'package:shopping_app/screens/constants.dart';
+
+import 'categories.dart';
+import 'item_card.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+          child: Text(
+            "Women",
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall
+                ?.copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Categories(),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+            child: GridView.builder(
+              padding: EdgeInsets.zero, // No additional padding inside the GridView
+              itemCount: products.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.75,
+              ),
+              itemBuilder: (context, index) => ItemCard(
+                product: products[index],
+                press: () {
+                },
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
